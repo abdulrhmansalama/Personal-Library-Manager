620e897a4880a858a977678c7f4249115dc817f2
Abdulrhman mohamed salama <154723658+abdulrhmansalama@users.noreply.github.com>
2025-03-24 16:12:14 +0200

    Update books.py

--- api/books.py
+++ api/books.py
@@ -1,40 +1,27 @@
 from http.server import BaseHTTPRequestHandler
-import json
 from lib.library import Library
 from lib.book import Book
+import json
 
 def handle_request(handler):
     lib = Library()
     
     if handler.command == 'GET':
-        books = [book.to_dict() for book in lib.books]
         handler.send_response(200)
         handler.send_header('Content-type', 'application/json')
         handler.end_headers()
-        handler.wfile.write(json.dumps(books).encode())
+        handler.wfile.write(json.dumps(lib.books).encode())
     
     elif handler.command == 'POST':
         content_length = int(handler.headers['Content-Length'])
         post_data = json.loads(handler.rfile.read(content_length))
         
-        new_book = Book(
-            title=post_data['title'],
-            author=post_data['author'],
-            genre=post_data['genre'],
-            publication_year=int(post_data['publication_year'])
-        )
-        
+        new_book = Book(**post_data)
         lib.add_book(new_book)
+        
         handler.send_response(201)
         handler.end_headers()
         handler.wfile.write(json.dumps({'status': 'success'}).encode())
-    
-    elif handler.command == 'DELETE':
-        title = handler.path.split('/')[-1]
-        lib.remove_book(title)
-        handler.send_response(200)
-        handler.end_headers()
-        handler.wfile.write(json.dumps({'status': 'deleted'}).encode())
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -42,12 +29,8 @@ class Handler(BaseHTTPRequestHandler):
     
     def do_POST(self):
         handle_request(self)
-    
-    def do_DELETE(self):
-        handle_request(self)
 
 def app(request):
     handler = Handler(request)
     handler.handle()
     return handler
-
